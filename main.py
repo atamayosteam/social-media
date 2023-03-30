9678f4a6c88334cf1a520070e54736a2ad866947
Vlad Dracula <vlad@tran.sylvan.ia>
2023-03-30 10:52:54 -0400

    s

--- main.py
+++ main.py
@@ -1,11 +1,49 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+
+from flask_login import loginManager
+
 import pymysql
 import pymysql.cursors
+
+login_manager = loginManager()
 
 
 
 
 app = Flask(__name__)
+login_manager.init_app(app)
+
+class User:
+    def __init__(self, id, username, banned):
+          self.is_authenticated = True
+          self.is_anonymous = False
+          self.is_active = not banned
+
+
+          self.username = username
+          self.id = id
+
+    def get_id(self):
+     return str(self.id)
+    
+
+@login_manager.user_loader
+def user_loader(user_id):
+     cursur = connection.cursur()
+
+     cursur.execute("SELECT * from `user` WHERE `id` =" + user_id)
+
+
+
+
+     result = cursur.fetchone()
+
+     if result is None:
+          return None
+     
+     return User(result['id'],result['username'],result['banned'])
+
+
 
 
 @app.route("/home")
@@ -34,22 +72,34 @@ def post_feed():
 
     )
 
-@app.route('/signin')
+@app.route('/sign-in')
 def sign_in():
      return render_template("signin.html.jinja")
 
-@app.route('/signup', methods=['POST', 'GET'])
+@app.route('/sign-up', methods=['POST', 'GET'])
 def sign_up():
     if request.method == 'POST':
           #Handle Signup
           cursur = connection.cursor()
 
+          photo = request.files['photo']
+
+          file_name = photo.filename # my_photo.jpg
+
+          file_extencion = file_name.split('.')[-1]
+
+          if file_extencion in ['jpg','jpeg','png','gif']:
+               photo.save('media/users' + file_name)
+          else:
+               raise Exception('Invalid file type')
+        
+
           cursur.execute("""
             INSERT INTO `user`(`username`,`password`,`email`,`display_name`,`bio`,`photo`)
             VALUES(%s,%s,%s,%s,%s,%s)
-          """)
+          """,(request.form['username'],request.form['password'],request.form['email'],request.form['display_name'],request.form['bio'],file_name))
 
-          return request.form
+          return redirect('/post')
     elif request.method == 'GET':
         return render_template("signup.html.jinja")
 
